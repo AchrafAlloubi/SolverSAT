9f43c8c3010ad8e0611941e9caa68446272557ae
Achraf ALLOUBI <achrafalloubi@gmail.com>
2022-06-04 19:59:57 -0400

    Logical conjunction

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import stev.booleans.Source.Core.src.stev.booleans.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
  // #26###81#3##7#8##64###5###7#5#1#7#9###39#51###4#3#2#5#1###3###25##2#4##9#38###46#
@@ -41,14 +42,13 @@ public class Main {
         // Export this formula as an array of clauses
         int[][] clauses = cnf.getClauses();
 
-        // What's in that array? First element corresponds to first clause: [1, -2, 3]
+
         System.out.println(Arrays.toString(clauses[0]));
-        // Second element corresponds to second clause: [1, -3, 4]
         System.out.println(Arrays.toString(clauses[1]));
 
-//        // What is the integer associated to variable q?
-//        Map<String,Integer> associations = cnf.getVariablesMap();
-//        System.out.println("Variable q is associated to number " + associations.get("q"));
+       // What is the integer associated to variable q?
+        Map<String,Integer> associations = cnf.getVariablesMap();
+        System.out.println("Variable X is associated to number " + associations.get("x"));
     }
 
     private static void boxContainsOnlyOneNumber(String Sudoku) {
